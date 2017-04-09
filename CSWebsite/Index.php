888b7c97f41841c3d1cd17adb3c73215a764e8a0
kevingeo971 <kevingeo971@gmail.com>
2017-04-09 16:13:35 +0530

    CSE Website
    Website For CSE Department RIT(Ramaiah Institute Of Technology)

---
+++ CSWebsite/Index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<head>
+    <title>msritcse</title>
+    <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css" type="text/css">
+    <script type="text/javascript" src="Bootstrap/js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="Bootstrap/js/bootstrap.min.js"></script>
+    <style>
+        .bl{
+            background-color: black;
+        }
+        
+        .navim{
+            padding-left: 20px;
+        }
+        
+        #icons{
+            float: bottom;
+        }
+        
+        .navbar-header{
+            color: orange;
+        }
+        
+        #nl{
+            padding-left: 50px;
+        }
+        
+        #r2{
+            padding-top: 30px;
+        }
+        
+        .hod{
+            padding-top: 50px;
+        }
+        
+        .ht{
+            font-size: 47px;
+            color:greenyellow;
+            border-bottom: solid black;
+            
+        }
+        
+        .hodcontent{
+            padding-top:50px; 
+        }
+        
+        .first{
+            padding-top: 120px;
+        }
+        
+        .dr{
+            color: gray;
+         
+        }
+        
+        .hodtext{
+            border-left:1px solid grey;
+        }
+        
+        #dept{
+            padding-top: 50px;
+            font-size: 60px;
+        }
+        
+        .abt{
+            padding-top: 20px;
+        }
+        
+        .poes{
+            font-size: 46px;
+            color:greenyellow;
+            border-bottom: solid black;
+        }
+        
+        .container{
+            background-color: white;
+        }
+        
+        .po{
+            background-color: grey;
+            color: white
+        }
+    </style>
+</head>
+
+<body class="bl">
+   
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-header">
+            <a href="#">
+                <img class="navim" src="IMAGES/cs_logo.png" alt="cs" height="80px" witdh="100px">
+                
+            </a>
+            <a href="#">
+                
+                <img class="navim"src="IMAGES/msrit_logo.png" alt="msrit" height="100px" witdh="120px">
+            </a>  
+        </div>
+        <ul class="nav navbar-nav navbar-right" id="icons">
+            <li>
+                <br>
+                <a href="#"><span class="glyphicon glyphicon-user "></span><b>  LOGIN</b></a>
+            </li>
+            <li>
+                <br>
+                <a href="#"><span class="glyphicon glyphicon-book"></span><b>  COURSES</b></a>
+            </li>
+            <li>
+                <br>
+                <a href="#"><span class="glyphicon glyphicon-calendar "></span><b>  EVENTS</b></a>
+            </li>
+            <li>
+                <br>
+                <a href="#"><span class="glyphicon glyphicon-bell "></span><b>   NOTIFICATIONS     </b></a>
+            </li>
+        </ul>    
+    </div>
+    
+    <div class="container">
+        <div class="row first ">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/cal1.png" alt="calender" height="150px"></a>
+            </div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/courses.png" alt="calender" height="150px"></a>
+            </div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/fac.png" alt="calender" height="150px"></a>
+            </div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/im3.png" alt="calender" height="150px"></a>
+            </div>
+            <div class="col-lg-2" id="nl">    
+                <a href="#"><img src="IMAGES/newsletter.jpg" alt="calender" height="150px"></a>
+            </div>
+        </div>
+        <div class="row " id="r2">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/pfc.png" alt="calender" height="150px"></a>
+            </div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/r2.png" alt="calender" height="150px"></a>
+            </div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/stud.png" alt="calender" height="150px"></a>
+            </div>
+            <div class="col-lg-2">    
+                <a href="#"><img src="IMAGES/time2.png" alt="calender" height="150px"></a>
+            </div>
+        </div>
+        <div class="row hod">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6 ht"><b>HEAD   OF   DEPARTMENT</b></div>
+        </div>
+        <div class="row hodcontent">
+            <div class="col-lg-6 dr">
+                <img src="IMAGES/ak_hod.jpg" height="450px">
+                <br><b>Dr. Anita Kanavalli, Head of the Department, Computer Science & Engineering</b>  
+            </div>
+            <div class="col-lg-6 hodtext">
+                <h3><b>Brief Profile:</b></h3>
+<br>Dr. Anita Kanavalli received her Ph.D. in Computer Science and Engineering from Bangalore University in 2013. She has published many research papers in International Conferences and Journals. She is a member of Indian Society of Technical Education (ISTE) and IEEE (Computer Society). She has guided many M.Tech students and is guiding PhD scholars. She is serving the institution from past 20+ years. She is also a certified instructor for CISCO Networking Academy for CCNA. Dr. Anita Kanavalli has a wide experience in teaching engineering students both in UG and in PG. She has guided many projects in UG and PG. She has been actively involved in Research work in the area of computer networks and Sensor network applications. She was also a Co-Investigator for a project "Setting Up a Sensor Network Testbed" funded by UGC New Delhi.
+<br>
+<br><b>Contact:
+<br>
+<br>Department of Computer Science & Engineering
+<br>M S Ramaiah Institute of Technology
+<br>MSRIT Post, MSR Nagar
+<br>Bangalore-560054
+<br>Email ID: anithak@msrit.edu
+<br>080-23600822/23606939, Extension: 140/141</b>
+
+
+
+            </div>
+        </div>
+        
+        <div class="row po" id="dept">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-5 ht"><b>THE   DEPARTMENT</b></div>
+        </div>
+        
+        <div class="row abt po">
+            The Department of Computer science and Engineering (CSE) was established in the year 1984 to meet the needs of the emerging IT industry and research. The department started with an initial intake of 60 students in UG programme in CSE and enhanced its intake to 120 in the year 1994. In 1998, the department started a PG programme in “Computer Science and Engineering (CSE)” with an intake of 18 students. Presently the department has two PG programs in the branch of Computer Science and Engineering and Computer Network Engineering with in-take of 30 and 18 students respectively.         
+            <br><b><h3>   VISION :</h3></b>
+            To build a strong learning and research environment in the field of Computer Science and Engineering that responds to the challenges of 21st century.
+            <br><b><h3>   MISSION :</h3></b>
+            <ul>
+                <li>To produce computer science graduates who, trained in design and implementation of computational systems through competitive curriculum and research in collaboration with industry and other organizations.
+                <li>To educate students in technology competencies by providing professionally committed faculty and staff.
+                <li>To inculcate strong ethical values, leadership abilities and research capabilities in the minds of students so as to work towards the progress of the society.</li>
+            </ul>
+        </div>
+        <div class="row" id="dept">
+            <div class="col-lg-0"></div>
+            <div class="col-lg-12 poes"><b>PROGRAMME EDUCATIONAL OBJECTIVES (PEOs)</b></div>
+        </div>
+        <div class="row">
+            <br>A B.E. (Computer Science & Engineering) graduate of M. S. Ramaiah Institute of Technology should, within three to five years of graduation :
+            <ol>
+                <li>Pursue a successful career in the field of Computer Science & Engineering or a related field utilizing his/her education and contribute to the profession as an excellent employee, or as an entrepreneur
+                <li>Be aware of the developments in the field of Computer Science & Engineering, continuously enhance their knowledge informally or by pursuing graduate studies</li>
+                <li>Be able to work effectively in multidisciplinary environments and be responsible members/leaders of their communities.</li>
+            </ol>
+        </div>
+        <div class="row po" id="dept">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8 ht"><b>PROGRAMME OUTCOMES (POs)</b></div>
+        </div>
+        <div class="row po">
+            <br>The outcomes of the Bachelor of Engineering in Computer Science & Engineering Programme are as follows: 
+            <br>A B.E. (Computer Science & Engineering) graduate must demonstrate :          
+            <ol>
+                <li>An ability to apply knowledge of mathematics, science, and engineering as it applies to Computer Science & Engineering</li>
+                <li>An ability to identify, formulate, study, and analyze and solve complex computing problems</li>
+                <li>An ability to design a computer-based system, component, software or process to meet the desired needs.</li>
+                <li>An ability to design and conduct experiments, evaluate results and provide valid conclusions.</li>
+                <li>An ability to use modern computing techniques, technologies and tools necessary for computing engineering practice.</li>
+                <li>An ability to understand and assess the societal, legal and security issues related to the practice of computer science and engineering.</li>
+                <li>An ability to understand the impact of computing solutions in an economic, environmental and societal context.</li>
+                <li>An understanding of professional and ethical responsibilities in professional engineering practice</li>
+                <li>An ability to function effectively individually and in team, and in multi-disciplinary environment.</li>
+                <li>An ability to communicate effectively.</li>
+                <li>An understanding of the engineering and management principles required for project and finance management.</li>
+                <li>Recognition of the need for, and an ability to engage in life-long learning.</li>
+            </ol>
+        </div>
+    </div>
+</body>
